892ba8df16f1e5abc5729affec483a7fd4bb4931
Rahul <rahulkumar397222@gmail.com>
2024-08-03 11:23:08 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2 as cv
+
+
+def test(x):
+    pass
+
+img = np.zeros((300,512,3), np.uint8)
+cv.namedWindow("Trackbars")
+
+cv.createTrackbar("R","Trackbars",0, 255, test)
+cv.createTrackbar("G","Trackbars",0, 255, test)
+cv.createTrackbar("B","Trackbars",0, 255, test)
+
+while True:
+    cv.imshow("Trackbars", img)
+    
+    k = cv.waitKey(1) & 0xFF
+    if k == 27:
+        break
+
+    r = cv.getTrackbarPos("R", "Trackbars")
+    g = cv.getTrackbarPos("G", "Trackbars")
+    b = cv.getTrackbarPos("B", "Trackbars")
+
+    hex_code = f'#{r:02x}{g:02x}{b:02x}'
+
+    print(hex_code)
+
+    cv.putText(img,hex_code,(100,500),cv.FONT_HERSHEY_SIMPLEX,2,(255,255,255))
+
+    img[:] = [b,g,r]
+
+
+
